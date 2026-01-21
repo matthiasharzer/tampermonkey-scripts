ca2ee199d9af006354ddc884934230409905e64b
matthiasharzer <matthiasharzer@users.noreply.github.com>
2026-01-21 15:41:11 +0000

    chore: Bump version to 0.2.0. [skip ci]

--- refresh-theme.user.js
+++ refresh-theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Theme Refresher (YT & Twitch)
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.2.0
 // @description  Refreshes YouTube and Twitch when system theme changes, maintaining video timestamp
 // @author       Matthias Harzer
 // @match        *://www.youtube.com/*
